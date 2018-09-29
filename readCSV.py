01a52544b9778f9039f9352f2813c9686dcb54dc
shayan <shayanadc@shayans-MacBook-Pro.local>
2018-09-29 17:19:54 +0330

    refactor to func

--- readCSV.py
+++ readCSV.py
@@ -1,20 +1,30 @@
-import csv,json
-pack = []
-with open('case.csv', newline='') as f:
-    reader = csv.reader(f)
-    for row in reader:
+import csv, json
+
+fileAddress = 'ip.csv'
+
+
+def getDataListFromCSV(fileAddress):
+    pack = []
+    with open(fileAddress, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
             pack.append(row)
-titles = pack[0]
-pack.remove(pack[0])
-d1 = {}
-d2 = {}
-for index in range(len(pack)):
-    i = 0
-    for x in pack[index]:
-        # print(index,titles[i],x)
-        d1[titles[i]] = x
-        # d[index][titles] = d[index][titles[i]][x]
-        d2[index] = d1
-        i = i+1
-# print(json.dumps(d2))
-print(json.dumps(d2, ensure_ascii=False))
+        return pack
+
+
+def bindListByTitle(column, dataList):
+    d1, d2 = {}, {}
+    for j in range(len(dataList)):
+        i = 0
+        for x in dataList[j]:
+            d1[column[i]], d2[j] = x, d1
+            i += 1
+    return d2
+def convertCSVFiletoJson(fileAddress):
+    pack = getDataListFromCSV(fileAddress)
+    titles = pack[0]
+    pack.remove(pack[0])
+    return bindListByTitle(titles, pack)
+
+dx = convertCSVFiletoJson(fileAddress)
+print(json.dumps(dx, ensure_ascii=False))
